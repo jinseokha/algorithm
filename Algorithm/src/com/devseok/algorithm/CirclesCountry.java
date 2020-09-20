762e4621c1fdb2063f666c2e35e5c24083e47663
jinseok <seok270@gmail.com>
2020-09-20 20:12:31 +0900

    둥근 모양의 국가들

--- Algorithm/src/com/devseok/algorithm/CirclesCountry.java
+++ Algorithm/src/com/devseok/algorithm/CirclesCountry.java
@@ -2,4 +2,18 @@ package com.devseok.algorithm;
 
 public class CirclesCountry {
 
+	public int leastBorders(int[] X, int[] Y, int[] R, int x1, int y1, int x2, int y2) {
+		int num = 0;
+		
+		for (int index = 0; index < X.length; index++) {
+			if(inside(X[index], Y[index], x1, y1, R[index]) != inside(X[index], Y[index], x2, y2, R[index])) {
+				num++;
+			}
+		}
+		return num;
+	}
+	
+	boolean inside(int x1, int y1, int x2, int y2, int r) {
+		return (x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2) <= r * r;
+	}
 }
