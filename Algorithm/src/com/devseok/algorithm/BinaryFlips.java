5f8624fd3d19a6efa41c7052244c3a2ed27e48ed
jinseok <seok270@gmail.com>
2020-09-24 20:02:19 +0900

    바이너리 플립2

--- Algorithm/src/com/devseok/algorithm/BinaryFlips.java
+++ Algorithm/src/com/devseok/algorithm/BinaryFlips.java
@@ -38,4 +38,46 @@ public class BinaryFlips {
 		}
 		return -1;
 	}
+	
+	int[] array;
+	int k;
+	Queue<Integer> q;
+	
+	public int minimalMoves2(int A, int B, int K) {
+		if (A == 0)
+			return 0;
+		if (A + B < K)
+			return -1;
+		
+		array = new int[A + B + 1];
+		
+		k = K;
+		
+		for (int i = 0; i <= A + B; i++) {
+			array[i] = -1;
+		}
+		
+		q = new LinkedList<Integer>();
+		q.add(A);
+		array[A] = 0;
+		
+		while (!q.isEmpty()) {
+			int i = q.poll();
+			
+			check(i, Math.max(0,  K - (A + B - i)));
+			check(i, Math.min(i , K));
+			check(i, Math.min(Math.max((i+1) / 2, K - ((A + B) - i)),  k));
+			check(i, Math.min(Math.max(i / 2,  K - ((A + B) - i)),  k));
+		}
+		
+		return array[0];
+	}
+	
+	public void check(int nowzero, int usezero) {
+		int nextzero = nowzero + k - 2 * usezero;
+		if (array[nextzero] == -1) {
+			array[nextzero] = array[nowzero] + 1;
+			q.add(nextzero);
+		}
+	}
 }
