e9e92f7d3dbf1b789c8822dc37acc3f19621af44
jinseok <seok270@gmail.com>
2020-10-27 21:20:30 +0900

    바이너리 플립3

--- Algorithm/src/com/devseok/algorithm/BinaryFlips.java
+++ Algorithm/src/com/devseok/algorithm/BinaryFlips.java
@@ -73,6 +73,36 @@ public class BinaryFlips {
 		return array[0];
 	}
 	
+	public int minimalMoves3(int A, int B, int K) {
+		if(A == 0) return 0;
+		if(A + B < K) return -1;
+		int minvalue = A, maxvalue = A;
+		
+		for (int i = 0; i <= A + B; i++) {
+			int nextminvalue, nextmaxvalue;
+			
+			if (minvalue <= K && maxvalue >= K) {
+				if (minvalue % 2 == K % 2) return i + 1;
+				else nextminvalue = 1;
+			} else {
+				nextminvalue = Math.min(Math.abs(K - minvalue), Math.abs(K - maxvalue));
+			}
+			
+			if (minvalue <= ( A+ B - K) && maxvalue >= (A + B - K)) {
+				if ((A + B - maxvalue) % 2 == K % 2) 
+					nextmaxvalue = A + B;
+				
+				else nextmaxvalue = A + B - 1;
+			} else {
+				nextmaxvalue = A + B - Math.min(Math.abs((A+B-K) - minvalue), Math.abs((A+B-K)-maxvalue));
+			}
+			
+			minvalue = nextminvalue;
+			maxvalue = nextmaxvalue;
+		}
+		return -1;
+	}
+	
 	public void check(int nowzero, int usezero) {
 		int nextzero = nowzero + k - 2 * usezero;
 		if (array[nextzero] == -1) {
